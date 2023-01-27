1ab2ce5e82f44efdfe3d6c084f9e11186a834e0a
PauRodriguezGuzman412 <pau.rodriguez@inslapineda.cat>
2023-01-27 08:44:04 +0100

    jose

---
+++ resources/views/layout/header.blade.php
@@ -0,0 +1,6 @@
+<meta charset="utf-8">
+{{-- <link href="{{ asset('/css/app.css') }}" rel="stylesheet">  --}}
+<link href="/css/app.css" rel="stylesheet"> 
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<header>Soy la cabecera</header>
+<hr>
