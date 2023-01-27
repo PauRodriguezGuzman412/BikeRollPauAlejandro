41625011865802e5c1129e1f7d75cfa434c568ef
Alex12671 <113857626+Alex12671@users.noreply.github.com>
2023-01-27 10:01:33 +0100

    aiuda

---
+++ database/migrations/2014_10_12_000000_create_pictures_table.php
@@ -0,0 +1,23 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+
+    public function up()
+    {
+        Schema::create('pictures', function (Blueprint $table) {
+            $table->id();
+            $table->foreign('course_id')->references('id')->on('courses')->onDelete('cascade');
+            $table->string('image_path');
+        });
+    }
+
+    public function down()
+    {
+        Schema::dropIfExists('pictures');
+    }
+};
