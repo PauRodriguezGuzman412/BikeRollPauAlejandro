0cf9ec8856bd7523eff40e7cc910f5721e0a661e
Alex <alejandro.gil@inslapineda.cat>
2023-02-06 13:20:07 +0100

    login (no funsiona B) )

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Admin;
+use Illuminate\Http\Request;
 use Illuminate\Routing\Controller as BaseController;
 
 class AdminController extends BaseController
@@ -9,5 +11,9 @@ class AdminController extends BaseController
     public function __invoke()
     {
         return view('adminIndex');
+    }
+    
+    public function validateAdminCredentials(Request $request, Admin $admin) {
+        print_r($request);
     }
 }
--- resources/views/adminIndex.blade.php
+++ resources/views/adminIndex.blade.php
@@ -6,9 +6,21 @@
     @include('layout.header')
 
 @section('component')
-    <a href="{{ route('courses') }}">Carreras</a><br>
-    <a href="{{ route('admin.aseguradoras.index') }}">Aseguradoras</a><br>
-    <a href="{{ route('sponsors') }}">Sponsors</a><br>
-    
+<link rel="stylesheet" href="{{ asset('css/app.css') }}" />
+<div class="container d-flex flex-column justify-content-start align-items-center adminFormDiv">
+    <h1 class="logAdminTitle">LOGIN ADMIN</h1>
+        <form method="POST" action="{{ route('admin.validate') }}" >
+            @csrf
+            <div class="mb-4">
+                <input type="text" name="username" placeholder="Escribe tu usuario...">
+            </div>
+            <div class="mb-4">
+                <input type="password" name="password" placeholder="Escribe tu contraseña...">
+            </div>
+            <div>
+                <button type="submit" class="submitLogAdminButton">INICIAR SESIÓN</button>
+            </div>
+        </form>
+</div>
 @section('footer')
     @include('layout.footer')
