15361f966d186eec4253e1d5b7797de24c886547
PauRodriguezGuzman412 <pau.rodriguez@inslapineda.cat>
2023-02-08 09:54:53 +0100

    Update CoursesController.php

--- app/Http/Controllers/CoursesController.php
+++ app/Http/Controllers/CoursesController.php
@@ -7,7 +7,7 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
 class CoursesController extends Controller
-{
+{    
     public function __invoke()
     {
         $courses= Courses::get();
