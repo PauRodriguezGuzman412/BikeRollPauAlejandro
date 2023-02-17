1851bcaa0b10e4e80530efc05030a64297e1b212
PauRodriguezGuzman412 <113432167+PauRodriguezGuzman412@users.noreply.github.com>
2023-02-17 10:17:38 +0100

    hover

--- resources/views/layout/header.blade.php
+++ resources/views/layout/header.blade.php
@@ -1,6 +1,4 @@
 <meta charset="utf-8">
-{{-- <link href="{{ asset('/css/app.css') }}" rel="stylesheet">  --}}
-{{-- <link href="/css/app.css" rel="stylesheet">  --}}
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
 <header>
