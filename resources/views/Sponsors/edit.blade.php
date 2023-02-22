4786a4205b6a9e5ea6ac3c0c60a585bff149eb8a
raulalcaldecabo <raul.alcalde@inslapineda.cat>
2023-02-22 08:56:01 +0100

    arreglado sponsors

--- resources/views/Sponsors/edit.blade.php
+++ resources/views/Sponsors/edit.blade.php
@@ -7,32 +7,45 @@
     @include('layout.nav')
 
 @section('component')
-
+</div>
+<div class="container d-flex flex-column justify-content-start align-items-center createRunnerformDiv">
+    <h1 class="formTitle">MODIFICAR SPONSOR</h1>
     <script src="{{ asset('js/components/modifyPicture.js')}}"></script>
     <form action="{{ route('sponsors.update', $sponsor['id']) }}" method="POST" enctype='multipart/form-data'>
         @csrf
         @method('PUT')
-
-        @foreach ($errors->all() as $error)
-            <div class="container">
-                <li class="alert alert-danger">{{ $error }}</li>
-            </div>
-        @endforeach
-        
-            {{-- test --}}
-        <input name="id"        type="hidden"     value="{{ old('id',$sponsor['id']) }}"><br>
-        <span>CIF</span>        <input name="CIF"                type="text"     value="{{ old('CIF',$sponsor['CIF']) }}"><br>
-        <span>Nombre</span>     <input name="nombre"              type="text"     value="{{ old('nombre',$sponsor['nombre']) }}" ><br>
-        <input type="file" name="logo" value="{{ $sponsor['logo'] }}">
-        <img src={{ $sponsor['logo'] }} alt="'Logo sponsor'"><br/>
-        <span>Modificar foto</span> <input id="Foto" type="checkbox" onclick="modificarFoto();"><br>
-        <div id="divFoto"></div>
-        {{-- <span>Logo</span>       <input name="logo"               type="file"               ><br> --}}
-        <span>Dirección</span>  <textarea name="address"         type="textarea"    >{{ $sponsor['address'] }}</textarea><br>
-        <input type="text" name="principal_page" value="0">
-        <span>1ª página</span>  <input name="principal_page"     type="checkbox"  value="{{ old('principal_page',$sponsor['principal_page']) }}"     ><br>
-        <button class="btn btn-info">Submit</button>
+        <input name="id"        type="hidden"     value="{{ old('id',$sponsor['id']) }}">
+        <div class="mb-4 runnerInput">
+            <span>CIF</span>
+            <input name="CIF" type="text" value="{{ old('CIF',$sponsor['CIF']) }}" placeholder="CIF">
+        </div>
+        <div class="mb-4 runnerInput">
+            <span>Nombre</span>
+            <textarea name="nombre" type="text" value="{{ old('nombre',$sponsor['nombre']) }}" placeholder="nombre">{{$sponsor['nombre']}}</textarea>
+        </div>
+        <div class="mb-4 runnerInput">
+            <span>Logo</span>
+            <input name="logo" type="file" value="{{ old('logo',$sponsor['logo']) }}" placeholder="Logo">
+        </div>
+        <div class="mb-4 runnerInput">
+            <span>Dirección</span>
+            <textarea name="address" type="text" value="{{ old('address',$sponsor['address']) }}" placeholder="Direccion">{{$sponsor['address']}}</textarea>
+        </div>
+        <div class="d-flex flex-column mb-4 checkBoxSponsorInput">
+            <span>1ª página</span> 
+            <input type="hidden" name="principal_page" value="{{ old('principal_page',$sponsor['principal_page']) }}">
+            <input name="principal_page" type="checkbox">
+        </div>
+        <button type="submit" class="submitLogAdminButton">MODIFICAR SPONSOR</button>
     </form>
-    
+    <div>
+        <a class="returnFormButton" href="{{ route('sponsors') }}">VOLVER</a>
+    </div>
+</div>
+@foreach ($errors->all() as $error)
+        <div class="container">
+            <li class="alert alert-danger">{{ $error }}</li>
+        </div>
+    @endforeach   
 @section('footer')
 @include('layout.footer')
