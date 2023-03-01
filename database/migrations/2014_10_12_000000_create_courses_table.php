4bcdd7843bf30941f412821104cd8e9010b54298
Alex12671 <113857626+Alex12671@users.noreply.github.com>
2023-03-01 08:28:26 +0100

    cdeers y eso

--- database/migrations/2014_10_12_000000_create_courses_table.php
+++ database/migrations/2014_10_12_000000_create_courses_table.php
@@ -12,7 +12,7 @@ return new class extends Migration
         Schema::create('courses', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->string('description');
+            $table->longText('description');
             $table->float('slope');
             $table->string('map_image');
             $table->string('maxim_participants');
